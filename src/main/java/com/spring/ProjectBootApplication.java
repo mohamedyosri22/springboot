7f818ee2d95415547e526becf07e94173ac3def5
mhmdyouali <mhmd.you@yahoo.com>
2022-12-04 17:05:27 +0200

    4th commit

--- src/main/java/com/spring/ProjectBootApplication.java
+++ src/main/java/com/spring/ProjectBootApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectBootApplication {
-
 	public static void main(String[] args) {
-
-
 		SpringApplication.run(ProjectBootApplication.class, args);
 	}
 
--- src/main/java/com/spring/controller/MainController.java
+++ src/main/java/com/spring/controller/MainController.java
@@ -1,20 +1,24 @@
 package com.spring.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// http://localhost:8080/
+// http://localhost:8080
 
 @RestController
 @RequestMapping("/api")
-// http://localhost:8080/api/
+// http://localhost:8080/api
 public class MainController {
 
-    // http://localhost:8080/api/fuck/
-    @GetMapping("/fuck")
-    public String sayFuck(){
-        return "fuck";
+    @Value("${admin.name}")
+    private String name;
+
+    // http://localhost:8080/api/hello
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello "+name;
     }
 }
