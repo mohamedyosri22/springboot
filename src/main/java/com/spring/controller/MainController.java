3a94cc64514de9f32ecbf482fa02fbf6195750ca
mhmdyouali <mhmd.you@yahoo.com>
2022-12-10 15:55:39 +0200

    Actuator

--- src/main/java/com/spring/controller/MainController.java
+++ src/main/java/com/spring/controller/MainController.java
@@ -6,26 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// http://localhost:8080
+// http://localhost:9090
 
 @RestController
 @RequestMapping("/api")
-// http://localhost:8080/api
+// http://localhost:9090/api
 public class MainController {
 
     @Value("${admin.name}")
     private String name;
 
-    // http://localhost:8080/api/hi
+    // http://localhost:9090/api/hi
     @GetMapping("/hi")
     public String sayHello(){
         return "hi "+name;
     }
 
-    // http://localhost:8080/api/fuck
-    @GetMapping("/fuck")
+    // http://localhost:9090/api/hello
+    @GetMapping("/hello")
     public String sayFuck(){
-        return "fuck "+name;
+        return "hello "+name;
     }
 
 }
